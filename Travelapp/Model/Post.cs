e8214c97c82583580eb315390ef38400c8602091
Samuel James <samueljames101101@gmail.com>
2020-03-26 20:15:06 +0000

    First commit

---
+++ Travelapp/Model/Post.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace Travelapp.Model
+{
+    public class Post
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Experience { get; set; }
+       
+    }
+}
